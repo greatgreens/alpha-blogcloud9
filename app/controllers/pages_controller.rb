7265d2e4ada54ac2a28cda0460a20884db14c5c9
Rails udemy <lennin20@hotmail.com>
2016-12-27 04:50:15 +0000

    Create rails app home and about pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+    
+    
+   def home
+       
+    
+    end
+    
+    
+    def about
+        
+        
+        
+        
+    end
+    
+    
+    
+    
+    
+end
